d082d4f85add5a0da983a843c9053c125f0cf60e
taylorschmidt <taylorhillschmidt@gmail.com>
2020-12-27 16:10:58 -0600

    create, delete, and get routes for comment made by entire group

--- backend/controllers/comments.controller.js
+++ backend/controllers/comments.controller.js
@@ -7,7 +7,6 @@ const Comment = db.comments
 
 exports.displayComments = (req, res) => {
     //find all cafes from certain cafeId
-
     Comment.find({CafeId: req.body.cafeId}).then(data=>{
         res.send(data)
     })
@@ -22,24 +21,18 @@ exports.addComment = (req, res) => {
     const CafeId = req.body.cafeId
     const UserId = req.body.userId
     const newComment = new Comment({
-        Content: Content
+        Content: Content,
+        UserId: UserId,
+        CafeId: CafeId
     })
-    
-        //save the new comment after finding in the cafe model where the ids are the same
-        Cafe.findById(CafeId).then((cafe)=> {
-            //add error here
-            newComment.CafeId.push(cafe._id)
-            save()
-            
-         })
-         //now, do the same for userId
-         User.findById(UserId).then((user)=> {
-             //add error here
-            newComment.UserId.push(user._id)
-            save()
-         })
-      
-        res.send(newComment)
+    newComment.save(newComment).then((data)=> {
+        res.send(data)
+    }).catch((err)=> {
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while creating the Comment"
+        })
+    })
 }
 
 
@@ -53,7 +46,7 @@ exports.deleteComments = (req, res) => {
     //delete a comment with id from model
     const userId = req.body.userId
     const id = req.body.id
-
+    //delete where user and id match commment
     Comment.findOneAndDelete({UserId: userId, _id: id}, function(err){
         if(err) {console.log(err)} else {console.log('successful deletion!')}
     }).then(data=>{
@@ -62,13 +55,4 @@ exports.deleteComments = (req, res) => {
 }
   
     
-    // Comment.find({UserId: userId}).then((data)=>{
-    //         Comment.findByIdAndRemove(id, function(err, data){
-    //             if(err) {
-    //                 console.log(err)
-    //             } else {
-    //                 res.send("removed data:", data)
-    //             }
-    //         })
-        // })
-    //if comment has certain user id, then allow for delete
+   
