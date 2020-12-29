91504b4ff57007d02b66e0889e4eec1acbbf275e
taylorschmidt <taylorhillschmidt@gmail.com>
2020-12-29 14:43:47 -0600

    cafe controller addCafe function editing

--- backend/controllers/cafe.controller.js
+++ backend/controllers/cafe.controller.js
@@ -1,37 +1,54 @@
 const db = require('../models/index')
-const { user } = require('../models/index')
+const { user, cafe } = require('../models/index')
 const Cafe = db.cafe
 const User = db.user
 //click add favorites adds Yelp Id to favorites array in user model and cafe model
 //make sure favorited cafe is the same across different users if multiple users like the same cafe
 //look at auth controller example on user
-exports.addCafe = (req, res) => {
-    //add a new cafe to model (model.create)
-    const YelpId = req.body.YelpId
-    //if yelp does not exist in db then run code below
-    const cafe = new Cafe ({
-        YelpId: YelpId
-    })   
-    //save cafe to current user
-    cafe.save(cafe)
-    //else push favorite to current user favorite
-    const id = req.body.id
 
-    // User.findByIdAndUpdate(id)
-    // .then(()=> {
-    //     User.favorites.push(YelpId)
-    //     res.send({message: "user favorites update"})
-    // })
-    User.findByIdAndUpdate(id, {$push: {"favorites": YelpId}},
-    {new: true}
-    )
-    .then((data)=>{
-        res.send(data)
-    })
-    .catch(err=>{
-        res.send(err)
-    })
+//original add cafe route
+// exports.addCafe = (req, res) => {
+//     //add a new cafe to model (model.create)
+//     const YelpId = req.body.YelpId
+//     //if yelp does not exist in db then run code below
+//     const cafe = new Cafe ({
+//         YelpId: YelpId
+//     })   
+//     //save cafe to current user
+//     cafe.save(cafe)
+//     //else push favorite to current user favorite
+//     const id = req.body.id
+
+//     User.findByIdAndUpdate(id, {$push: {"favorites": YelpId}},
+//     {new: true}
+//     )
+//     .then((data)=>{
+//         res.send(data)
+//     })
+//     .catch(err=>{
+//         res.send(err)
+//     })
+// }
+
+//just trying to add a cafe without the user model
+exports.addCafe = (req, res) => {
+        //add a new cafe to model (model.create)
+        const YelpId = req.body.YelpId
+        //if yelp does not exist in db then run code below
+        const cafe = new Cafe ({
+            YelpId: YelpId
+        })   
+        //save cafe to current user
+        cafe.save(cafe)
+        .then((data)=>{
+            res.send(data)
+        })
+        .catch(err=>{
+            res.send(err)
+        })
 }
+
+
 
 // exports.allCafes = (req,res) => {
 //     Cafe.find()
