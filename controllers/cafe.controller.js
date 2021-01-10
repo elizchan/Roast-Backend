1de66c16b5f03a29ffd29a7148458da851f88ec5
taylorschmidt <taylorhillschmidt@gmail.com>
2021-01-10 15:09:59 -0700

    no duplicate cafes

--- controllers/cafe.controller.js
+++ controllers/cafe.controller.js
@@ -9,32 +9,44 @@ const User = db.user
  
 //add new cafe and yelp information
 exports.addCafe = (req, res) => {
-        const YelpId = req.body.YelpId
-        const Name = req.body.Name
-        const Address = req.body.Address
-        const City = req.body.City
-        const Rating = req.body.Rating
-        const ImageURL = req.body.ImageURL
-        const YelpURL = req.body.YelpURL
+    const YelpId = req.body.YelpId;
+    const Name = req.body.Name;
+    const Address = req.body.Address;
+    const City = req.body.City;
+    const Rating = req.body.Rating;
+    const ImageURL = req.body.ImageURL;
+    const YelpURL = req.body.YelpURL;
+    Cafe.findOne({ YelpId: YelpId }, function (err, cafe) {
+      if (err) {
+        console.log(err);
+      }
+      if (cafe) {
+        // console.log("this cafe already exists in the database")
+        return;
+      } else {
         const newCafe = new Cafe({
-            YelpId,
-            Name,
-            Address,
-            City, 
-            Rating, 
-            ImageURL,
-            YelpURL
-        }, {unique:true})
-        newCafe.save((err, cafe)=>{
-            if (err) {
-                res.status(500).send({message: err})
-                return
-            } else {
-                res.send(cafe)
-                console.log(cafe, "cafe saved")
-            }
-        })
-}
+          YelpId,
+          Name,
+          Address,
+          City,
+          Rating,
+          ImageURL,
+          YelpURL,
+        });
+        newCafe.save((err, cafe) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          } else {
+            res.send(cafe);
+            // console.log(cafe, "cafe saved");
+          }
+        });
+      }
+    });
+  };
+  
+
 
 
 exports.findFavorite = (req, res) =>{
